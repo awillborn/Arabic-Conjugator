79df6bc00534f0747c8ed3ceac25884827a69204
Anne Willborn <awillborn@gmail.com>
2014-04-06 15:49:25 -0700

    Remove Affixes from Form class

--- lib/arabic_conjugator/form.rb
+++ lib/arabic_conjugator/form.rb
@@ -1,8 +1,3 @@
-# encoding: utf-8
-
-PAST_AFFIXES = { :I => "ت", :you_m => "ت", :you_f => "ت", :she => "ت" , :he =>'', :we => "نا", :you_pl => "تم", :they => "وا" }
-PRESENT_AFFIXES = { :I => ["أ", ''], :you_m => ["ت", ''], :you_f => ["ت", "ين"], :he => ["ي", ""], :she => ["ت", ""], :we => ["ن", ''], :you_pl => ["ت", "ون"], :they => ["ي", "ون"] }
-
 class Form
   attr_reader :base, :root1, :root2, :root3
 
