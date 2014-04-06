d02272e8c8b216410534c06deb44b197188205a6
Anne Willborn <awillborn@gmail.com>
2014-04-06 12:01:30 -0700

    Form VII hollow past specs

--- spec/formVII_spec.rb
+++ spec/formVII_spec.rb
@@ -4,21 +4,33 @@ require 'spec_helper'
 
 describe "Form VII" do
 
-  context "#past" do
+  context "regular past" do
     it 'conjugates formVII past' do
       verb = Verb.new({root1: "ق", root2: "ط", root3: "ع", form: "7", tense: "past", pronoun: :she})
       expect(verb.conjugate).to eq("انقطعت")
     end
   end
 
-  context "#defective_past" do
+  context "defective past" do
     it 'conjugates formVII defective past' do
       verb = Verb.new({root1: "ق", root2: "ض", root3: "ي", form: "7", tense: "past", pronoun: :she})
       expect(verb.conjugate).to eq("انقضت")
     end
   end
 
-  context '#present' do
+  context "hollow past" do
+    it 'conjugates formVII past, :she' do
+      verb = Verb.new({root1: "ق", root2: "و", root3: "د", form: "7", tense: "past", pronoun: :she})
+      expect(verb.conjugate).to eq("انقادت")
+    end
+
+    it 'conjugates formVII past, :you_pl' do
+      verb = Verb.new({root1: "ق", root2: "و", root3: "د", form: "7", tense: "past", pronoun: :you_pl})
+      expect(verb.conjugate).to eq("انقدتم")
+    end
+  end
+
+  context 'regular present' do
     it 'conjugates formVII present' do
       verb = Verb.new({root1: "ق", root2: "ط", root3: "ع", form: "7", tense: "present", pronoun: :she})
       expect(verb.conjugate).to eq("تنقطع")
