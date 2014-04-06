6dd67f239e9ccbe7ef12cc35c536162be6411788
Anne Willborn <awillborn@gmail.com>
2014-04-06 14:04:14 -0700

    Form VIII doubled past specs and implementation

--- spec/formVIII_spec.rb
+++ spec/formVIII_spec.rb
@@ -4,48 +4,60 @@ require 'spec_helper'
 
 describe "Form VIII" do
 
-  context "#past" do
-    it 'conjugates formVIII past' do
+  context "regular past" do
+    it 'conjugates form VIII past' do
       verb = Verb.new({root1: "ك", root2: "ش", root3: "ف", form: "8", tense: "past", pronoun: :I})
       expect(verb.conjugate).to eq("اكتشفت")
     end
   end
 
-  context "#defective_past" do
-    it 'conjugates formVIII defective past' do
+  context "defective past" do
+    it 'conjugates form VIII defective past' do
       verb = Verb.new({root1: "ب", root2: "ل", root3: "ي", form: "8", tense: "past", pronoun: :I})
       expect(verb.conjugate).to eq("ابتليت")
     end
   end
 
-  context "#hollow_past" do
-    it 'conjugates formVIII hollow past' do
+  context "hollow past" do
+    it 'conjugates form VIII hollow past' do
       verb = Verb.new({root1: "خ", root2: "و", root3: "ر", form: "8", tense: "past", pronoun: :I})
       expect(verb.conjugate).to eq("اخترت")
     end
   end
 
-  context "#assimilated_past" do
-    it 'conjugates formVIII assimilated past with root1 waaw' do
+  context "doubled past" do
+    it 'conjugates form VIII doubled past, :he' do
+      verb = Verb.new({root1: "م", root2: "د", root3: "د", form: "8", tense: "past", pronoun: :he})
+      expect(verb.conjugate).to eq("امتدّ")
+    end
+
+    it 'conjugates form VIII doubled past, :I' do
+      verb = Verb.new({root1: "م", root2: "د", root3: "د", form: "8", tense: "past", pronoun: :I})
+      expect(verb.conjugate).to eq("امتددت")
+    end
+  end
+
+  context "assimilated past" do
+    it 'conjugates form VIII assimilated past with root1 waaw' do
       verb = Verb.new({root1: "و", root2: "ف", root3: "ق", form: "8", tense: "past", pronoun: :they})
       expect(verb.conjugate).to eq("اتّفقوا")
     end
 
-    xit 'conjugates formVIII assimilated past with root1 Thal' do
+    xit 'conjugates form VIII assimilated past with root1 Thal' do
       verb = Verb.new({root1: "ظ", root2: "ل", root3: "م", form: "8", tense: "past", pronoun: :he})
       expect(verb.conjugate).to eq("اظّلم")
     end
   end
 
-  context '#present' do
-    it 'conjugates formVIII present' do
+  context 'regular present' do
+    it 'conjugates form VIII present' do
       verb = Verb.new({root1: "ك", root2: "ش", root3: "ف", form: "8", tense: "present", pronoun: :I})
       expect(verb.conjugate).to eq("أكتشف")
     end
   end
 
-  context '#assimilated_present' do
-    it 'conjugates formVIII assimilated present' do
+  context 'assimilated present' do
+    it 'conjugates form VIII assimilated present' do
       verb = Verb.new({root1: "و", root2: "ف", root3: "ق", form: "8", tense: "present", pronoun: :they})
       expect(verb.conjugate).to eq("يتّفقون")
     end
